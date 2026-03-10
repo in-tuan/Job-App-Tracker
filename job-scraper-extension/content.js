ad848e58641b8fd82abb3889e77f39765370b70e
Inaara <intuan21@outlook.com>
2026-03-10 13:31:37 -0400

    feat(extension): created popup, scrapes data from waterlooworks page and posted to database BUG: does not update if any status changes

---
+++ job-scraper-extension/content.js
@@ -0,0 +1,41 @@
+// content.js
+function scrapeJobs() {
+  const rows = document.querySelectorAll('#dataViewerPlaceholder table tbody tr');
+  
+  const jobs = Array.from(rows).map(row => {
+    const appStatus = row.querySelector('td:nth-child(5) span span')?.innerText.trim();
+    const jobStatus = row.querySelector("td:nth-child(6) span span")?.innerText.trim();
+    
+    let cleanStatus = "Pending";
+    if ((jobStatus === "Interview Selections Complete" || jobStatus === "Interview Complete"
+        || jobStatus === "Emp Rankings Finalized")
+         && appStatus === "Applied") {
+        cleanStatus = "Not Selected";
+    } else if (appStatus === "Not Selected") {
+        cleanStatus = "Not Selected";
+    } else if (jobStatus === "Stalled" || jobStatus === "Cancel") {
+        cleanStatus = "N/A";
+    }
+
+    return {
+      job_id: row.querySelector("td:nth-child(2)")?.innerText.trim(),
+      job_title: row.querySelector("th a")?.innerText.trim(),
+      organization: row.querySelector("td:nth-child(4)")?.innerText.trim(),
+      app_status: appStatus,
+      job_status: jobStatus,
+      location: row.querySelector("td:nth-child(8)")?.innerText.trim(),
+      date_submitted: row.querySelector("td:nth-child(12)")?.innerText.trim(),
+      clean_status: cleanStatus,
+      source: 'internal'
+    };
+  });
+
+  return jobs;
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "get_jobs") {
+    const data = scrapeJobs();
+    sendResponse({ data: data });
+  }
+});
